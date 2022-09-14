b1629c523a1687636311155dc1b936e4ff04db4d
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2022-09-14 11:47:45 -0300

    Update Te.py

--- o aula/Te.py
+++ o aula/Te.py
@@ -1,1 +1,8 @@
+# 1. Desenvolva um programa que verifique e mostre os números
+# entre 1.000 e 2.000 que, quando
+# divididos por 11, produzam o resto igual a 5.
+
+for num in range(2, 6):
+    print(num)
+
 print('uso nome saudação')
