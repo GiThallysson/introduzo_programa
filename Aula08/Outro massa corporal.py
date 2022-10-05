9a11eb3175f72d4dfdd3e4e0cc84ad70f28502ec
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2022-10-05 10:42:43 -0300

    Update Outro massa corporal.py

--- Aula08/Outro massa corporal.py
+++ Aula08/Outro massa corporal.py
@@ -1,6 +1,8 @@
 # 01 - [FORBELLONE, 2022] Construa um algoritmo para calcular as raízes de uma equação do 2 grau (Ax² + Bx + C), 
 # sendo que os valores A, B, C são fornecidos pelo usuário. (considere que a equação possui duas raizes reais).
  
+# nome Thallysson Costa Viana Amorim  
+ 
 a = int(input("Digite um valor para A: "))
 b = int(input("Digite um valor para B: "))
 c = int(input("Digite um valor para C: "))
