8679f73dc71da4117139309c23077dbe7ad2811f
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2022-10-21 14:29:22 -0300

    Update 7 q.py

--- Aula 10/7 q.py
+++ Aula 10/7 q.py
@@ -1,3 +1,9 @@
+#  7. Faça um algoritmo para ler dois vetores V1
+   # e V2 de 10 números cada. Calcular e escrever
+   # a quantidade de vezes que V1 e V2 possuem os
+   # mesmos números e nas mesmas posições.
+#
+
 from random import randint
 
 v1 = []
