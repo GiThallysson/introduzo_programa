81e3e963cdfe28d0ac28bef961a6c20b377ccc9b
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2022-11-04 19:20:24 -0300

    Create dicionário 1 q 2q nome favorito cidade.py

---
+++ Aula 9/dicionário 1 q 2q nome favorito cidade.py
@@ -0,0 +1,13 @@
+#1. Use um dicionário para armazenar #informações sobre uma pessoa que você conheça. Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive. Você deverá ter chaves como # primeiro_nome, sobrenome, idade, e cidade. Por fim, mostre cada informação armazenada em seu dicionário.
+
+g = {'cinto':' João Pedro 24 ano o idade cidade Recife'}
+g['cinto']
+print(g)
+
+#2. Use um dicionário para armazenar os #números favoritos de algumas pessoas. Esoclha cinco colegas, e pergunte quais seus número favoritos. Use seus nomes para serem #as chaves de cada número favorito. Ao final, exiba o nome de cada pessoas e seu número favorito.
+
+g = {'nome':' Gustavo 26 número Maria 31 número José 33 número Roberto 34 número Lucas 35 número '}
+g['nome']
+print(g)
+
+ 
