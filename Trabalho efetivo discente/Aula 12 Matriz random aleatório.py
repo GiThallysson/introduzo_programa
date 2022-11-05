9b536d13105af6ebbaa82b6c423c22061fdc02fd
GiThallysson <112260975+GiThallysson@users.noreply.github.com>
2022-11-05 18:07:34 -0300

    Create Aula 12 Matriz random aleatório.py

---
+++ Trabalho efetivo discente/Aula 12 Matriz random aleatório.py
@@ -0,0 +1,22 @@
+from random import randint
+
+A = []
+B = []
+
+for lista in range(10):
+    lista = []
+
+    for coluna in range(10):
+        lista.append(randint(0, 100))
+       
+    A.append(lista)
+    B.append(lista)
+    
+for lista_matriz in A:
+    print(lista_matriz)
+    
+print('\n')
+
+for list1 in B:
+        print(list1)
+        
